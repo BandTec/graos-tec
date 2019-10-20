7da53e279240a79be3c39ae9a437e27f2878c0f3
José Nilton Alves Teixeira Junior <joseniltonbandtec@users.noreply.github.com>
2019-10-20 18:34:37 -0300

    Arrumei a validação do apgina grãos

--- Site/vincularJs/cadastror_grãos.js
+++ Site/vincularJs/cadastror_grãos.js
@@ -4,13 +4,13 @@
         var umidade = umidade_validacao.value;
 
     if(grao == "" || grao.length <3) {
-        mensagem_grao.innerHTML += `<span style="color: #a30f0f;"> Preencha o campo <b>Grão</b></span>`;
+        mensagem_grao.innerHTML = `<span style="color: #a30f0f;"> Preencha o campo <b>Grão</b></span>`;
     }
     if(temperatura == "" || temperatura.length <3)  { /*3 pq são 4 digitos Exp:30.00 graus C*/
-        mensagem_temperatura.innerHTML += `<span style="color: #a30f0f;"> Preencha o campo <b>Temperatura</b></span>`;
+        mensagem_temperatura.innerHTML = `<span style="color: #a30f0f;"> Preencha o campo <b>Temperatura</b></span>`;
     }
     if(umidade == "" || umidade.length < 3) { /*3 pq são 4 digitos Exp:30.00 UR*/
-        mensagem_umidade.innerHTML += `<span style="color: #a30f0f;"> Preencha o campo <b>Umidade</b></span>`;
+        mensagem_umidade.innerHTML = `<span style="color: #a30f0f;"> Preencha o campo <b>Umidade</b></span>`;
     }
     else{
         alert(`Você esta cadastrador`);

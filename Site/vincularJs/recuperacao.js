a8ed86e38d8221d2e97b9daf647db203f17be378
José Nilton Alves Teixeira Junior <joseniltonbandtec@users.noreply.github.com>
2019-10-21 17:07:57 -0300

    modifiquei o js de recuperação

--- Site/vincularJs/recuperacao.js
+++ Site/vincularJs/recuperacao.js
@@ -1,11 +1,16 @@
-function refefinir(){
+function redefinir(){
     var email = email_validar.value;
 
-    if(email == 'oi'){
+    if(email.endsWith('@graostech.com')){
 
-        div_msg.innerHTML = '';
+        div_msg.innerHTML = '<span style="color: darkgreen">Aguarde um código para recuperação</span>';
 
     } else {
+
+        div_msg.innerHTML = '<span style="color: red">Seu e-mail não está cadastrado</span>'
+    }
+
+    if(email == ''){
 
         div_msg.innerHTML = '<span style="color: red">Preencha o campo e-mail</span>'
     }

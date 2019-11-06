1448cf1fd1e51266b5a9eb3cb27704d3c7c6ab18
José Nilton Alves Teixeira Junior <joseniltonbandtec@users.noreply.github.com>
2019-11-06 17:57:05 -0300

    Validando a pagina cadsasro pessoa ainda ;-;

--- novo site/js/validação-caddastro-pessoa.js
+++ novo site/js/validação-caddastro-pessoa.js
@@ -9,31 +9,56 @@ function validacao_cadastro_pessoal(){
     var email = valor_email.value;
     var senha1 = valor_senha1.value;
     var senha2 = valor_senha2.value;
-    var telefone = valor_telefone.value;
+    var telefone = Number(valor_telefone.value);
 
-    if(nome == "" || email == "" || email.indexOf ("@") ==-1 || telefone == "" || telefone.length <2 || senha1.length <=8 || senha1.length >=19 
-    || senha1 == senha2 || senha2 == ""){
-        if(nome == ""){
+    if(nome == "" || nome.length >=20 || nome.length <=2 || email.length <=10 || email.length >=40 || email.indexOf ("@") == -1 || email == "" ||
+    telefone == "" || telefone.length <2 || telefone.length >11 || senha1 == "" || senha1.length <=8 || senha1.length >=19 || 
+    senha1 == senha2 || senha2 == ""){
+        
+        if(nome.length >=30 || nome.length <=2){
             valida_nome.style.color = 'red';
-            valida_nome.innerHTML = `Nome Inválido`;
+            valida_nome.innerHTML = `Campo Inválido`;
+                if(nome == ""){
+                valida_nome.style.color = 'red';
+                valida_nome.innerHTML = `Nome vazio`;
+            }
         }
-        if(email == "" || email.indexOf ("@") == -1){
+
+        if(email.length <=10 || email.length >=40 || email.indexOf ("@") == -1){
             valida_email.style.color = 'red';
-            valida_email.innerHTML = `Email Inválido`;
+            valida_email.innerHTML = `Campo inválido`;
+                if(email == ""){
+                valida_email.style.color = 'red';
+                valida_email.innerHTML = 'Email vazio';
+            }
         }
-        if(telefone == "" || telefone.length <=9 || telefone.length >11){
+
+        if(telefone == ""){
             valida_telefone.style.color = 'red';
-            valida_telefone.innerHTML = `Telefone Inváldio`;
+            valida_telefone.innerHTML = `Telefone vazio`;
+                if(telefone.length <=9 || telefone.length >11){
+                    valida_telefone.style.color = 'red';
+                    valida_telefone.innerHTML = `Telefone inválido`;
+                }
         }
         if(senha1.length <=8 || senha1.length >=19){
             valida_senha.style.color = 'red';
             valida_senha.innerHTML = `Senha Invílida`;
+                if(senha1 == ""){
+                    valida_senha.style.color = 'red';
+                    valida_senha.innerHTML = `Senha Vazia`
+                }
         }
         if(senha1 != senha2){
             valida_confirmacao_senha.style.color = 'red';
             valida_confirmacao_senha.innerHTML = `Senha não esta igual`;
+                if(senha2 == ""){
+                    valida_confirmacao_senha.style.color = 'red';
+                    valida_confirmacao_senha.innerHTML = "Senha vazia";
+                }
         }
     }else{
+            /*Fazer a programção do banco aqui*/
         location.href = `../corpo/DashBoard.html`;
     }
 }

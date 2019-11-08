d843f1f010db9eea7be33442e323c9dcfb2d9e6e
José Nilton Alves Teixeira Junior <joseniltonbandtec@users.noreply.github.com>
2019-11-08 11:56:22 -0300

    Pagina login finalizada

--- novo site/js/login.js
+++ novo site/js/login.js
@@ -1,35 +1,29 @@
 function entrar(){
-    var login_valor = usuario_validar.value;
-    var senha_valor = senha_validar.value;
+    div_validar_email.innerHTML = "";
+    div_validar_senha.innerHTML = "";
 
+    var email = email_validar.value;
+    var senha = senha_validar.value;
 
-    if(login_valor == ''){
-
-        div_validar_login.innerHTML = '<span style="color: red">preencha campo usuario</span>';
-    }
-    else
-
-    if(login_valor == 'graos@tech.com'){
-
-        div_validar_login.innerHTML = '<span style="color: darkgreen">login válido</span>';
-
-    } else {
-
-        div_validar_login.innerHTML = '<span style="color: red">login inválido</span>';
-    }
-
-
-    if(senha_valor == ''){
-        
-        div_validar_senha.innerHTML = '<span style="color: red">preencha campo senha</span>';
-    }    
-     else if(senha_valor == '1234'){
-
-        div_validar_senha.innerHTML = '<span style="color: darkgreen">senha válida</span>';
-
-    } else {
-
-        div_validar_senha.innerHTML = '<span style="color: red">senha inválida</span>';
-    }
-
+    if(senha.length <=8 || senha.length >=19 || email.length <=10 || email.length >=40 || email.indexOf ("@") == -1 || email == "" ||
+    senha == ""){
+    if(senha.length <=8 || senha.length >=19){
+        div_validar_senha.style.color = 'red';
+        div_validar_senha.innerHTML = `Senha Inválida`; 
+            if(senha == ""){
+                div_validar_senha.style.color = 'red';
+                div_validar_senha.innerHTML = `Senha vazia`; 
+            }
+        }
+    if(email.length <=10 || email.length >=40 || email.indexOf ("@") == -1){
+        div_validar_email.style.color = 'red';
+        div_validar_email.innerHTML = `Senha Inválida`; 
+            if(email == ""){
+                div_validar_email.style.color = 'red';
+                div_validar_email.innerHTML = `E-mail vazio`; 
+            }
+        }
+    }else{
+        location.href = `./DashBoard.html`;
+    }   
 }
